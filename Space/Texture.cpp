c7f2bfc03afc80bf40a6407a6c44a3ec58fffe6b
8489545 <rebob9545@gmail.com>
2020-06-17 16:16:17 +0900

    칼라키 추가

--- Space/Texture.cpp
+++ Space/Texture.cpp
@@ -16,9 +16,10 @@ Texture::~Texture()
 
 bool Texture::Init(std::wstring fileName)
 {
+	D3DCOLOR ColorKey = D3DCOLOR_XRGB(0, 248, 0);
 	HRESULT hr;
 	hr = D3DXCreateTextureFromFileEx(m_pDev, fileName.c_str(), D3DX_DEFAULT_NONPOW2, D3DX_DEFAULT_NONPOW2, 1, 0,
-		D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, 0, 0, 0, &m_pTexture);
+		D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, D3DX_DEFAULT, D3DX_DEFAULT, ColorKey, 0, 0, &m_pTexture);
 
 	if FAILED(hr)
 	{
